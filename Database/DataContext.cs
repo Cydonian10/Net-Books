5f20799f7f6b42ac35d8a2021687908bba9881db
Gabriel <cydonian_07@hotmail.com>
2024-01-10 10:08:24 -0500

    Agregamos Autor a Entidades y al context

--- Database/DataContext.cs
+++ Database/DataContext.cs
@@ -5,10 +5,18 @@ namespace BookApi.Database;
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions options) : base(options)
-    {}
+    { }
+
+    public DbSet<Autor> Autores { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Autor>(e =>
+        {
+            e.ToTable("autores");
+            e.HasKey(e => e.Id);
+        });
+
         base.OnModelCreating(modelBuilder);
     }
 }
---
+++ Database/Entities/Autor.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookApi.Database;
+
+public class Autor : IId
+{
+  public int Id { get; set; }
+
+  [MaxLength(120)]
+  public string? Nombre { get; set; }
+}
