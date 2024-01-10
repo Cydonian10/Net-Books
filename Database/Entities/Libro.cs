3b60e28d08779909223a2671d69c7a23fc96ff70
Gabriel <cydonian_07@hotmail.com>
2024-01-10 14:46:23 -0500

    Implementamos el validador de letra mayuscula2

--- Database/Entities/Libro.cs
+++ Database/Entities/Libro.cs
@@ -1,8 +1,12 @@
-﻿namespace BookApi.Database;
+﻿using BookApi.Validators;
+
+namespace BookApi.Database;
 
 public class Libro : IId
 {
   public int Id { get; set; }
+
+  [PrimeraLetraMayuscula]
   public string? Titulo { get; set; }
   public int AutorId { get; set; }
   public Autor? Autor { get; set; }
