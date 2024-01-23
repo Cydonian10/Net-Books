04f654c53e4d787a85c46fc7d9b07e53ccf7b2fe
Admer <cydonian_07@hotmail.com>
2024-01-23 11:55:00 -0500

    Migraciones de autorlibro

---
+++ Migrations/20240123165203_AutorLibro.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AutorLibro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "autor_libro",
+                columns: table => new
+                {
+                    AutorId = table.Column<int>(type: "integer", nullable: false),
+                    LibroId = table.Column<int>(type: "integer", nullable: false),
+                    Orden = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_autor_libro", x => new { x.AutorId, x.LibroId });
+                    table.ForeignKey(
+                        name: "FK_autor_libro_autores_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "autores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_autor_libro_libros_LibroId",
+                        column: x => x.LibroId,
+                        principalTable: "libros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_autor_libro_LibroId",
+                table: "autor_libro",
+                column: "LibroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "autor_libro");
+        }
+    }
+}
